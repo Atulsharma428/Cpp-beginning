4839bd159d44fd6edbb98fa28a572ebd39d0100a
Atul Sharma <83007914+Atulsharma428@users.noreply.github.com>
2021-10-27 18:11:28 +0530

    c

---
+++ Programs/second.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+// cout << "Hello world!" << endl;
+
+// cout << "Hello" << " world!" << endl;
+
+// cout << "Hello world!\n";
+
+// cout << "Hello\nout\nthere\n";
+
+int num1;
+int num2;
+double num3;
+
+// cout << "enter an integer!";
+// cin >> num1;
+// cout << " you entered: " << num1 << endl;
+
+//cout << "Entre your first integer: "; 
+//cin >> num1;
+//cout << "Entre your second integer: "; 
+//cin >>num2; 
+//cout << "You entered " << num1 << " and " <<num2<< endl;
+
+// cout << "enter 2 integers with seprated with a space ";
+// cin >> num1 >> num2;
+// cout << "you entered: " << num1 << " and " << num2 << endl;
+
+// cout << "Enter a double: ";
+// cin >> num3;
+// cout << "You entered: " << num3 << endl; 
+
+cout << "Enter an integer here: ";
+cin >> num1;
+cout << "Enter a double here: ";
+cin >> num3;
+cout << "The integer is: " << num1 << endl;
+cout << "The double is: " << num3 << endl;
+
+
+return 0;
+}
